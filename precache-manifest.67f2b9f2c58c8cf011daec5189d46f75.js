8bc9b1b4027256f885989f29c7c492fcc3c6ce24
Dma0210 <easyhtmlandcss@gmail.com>
2020-08-19 12:55:06 +0300

    Updates

---
+++ precache-manifest.67f2b9f2c58c8cf011daec5189d46f75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6913b09d4998cff0bfe74358f477127",
+    "url": "/infobox-dm/index.html"
+  },
+  {
+    "revision": "7f2653ef8c424e3c2bdb",
+    "url": "/infobox-dm/static/css/main.80a05320.chunk.css"
+  },
+  {
+    "revision": "4b3e97a937099ce18907",
+    "url": "/infobox-dm/static/js/2.473ee69e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/infobox-dm/static/js/2.473ee69e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f2653ef8c424e3c2bdb",
+    "url": "/infobox-dm/static/js/main.e760e407.chunk.js"
+  },
+  {
+    "revision": "0d988d287da9c0e6abe0",
+    "url": "/infobox-dm/static/js/runtime-main.85c2895d.js"
+  }
+]);
